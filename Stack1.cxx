2cdd6da7428ba883ae9df2b11f9a3523fb3755de
Aditya Gade <53152137+Aditya-31@users.noreply.github.com>
2019-08-28 21:57:58 +0530

    Update Stack1.cxx

--- Stack1.cxx
+++ Stack1.cxx
@@ -1,3 +1,17 @@
+/*A palindrome is a string of character that‘s the same forward and backward. Typically,
+punctuation, capitalization, and spaces are ignored. For example, ‖Poor Dan is in a droop‖ is
+a palindrome, as can be seen by examining the characters ―poor danisina droop‖ and
+observing that they are the same forward and backward. One way to check for a
+palindrome is to reverse the characters in the string and then compare with them the
+
+original-in a palindrome, the sequence will be identical. Write C++ program with functions-
+1. To check whether given string is palindrome or not that uses a stack to determine whether
+
+a string is a palindrome.
+2. to remove spaces and punctuation in string, convert all the Characters to lowercase, and
+then call above Palindrome checking function to check for a palindrome
+3. to print string in reverse order using stack*/
+/*NAME: ADITYA GADE 	Class: SE-A(COMP)	Date of creation:26-08-2018		Last Updated: 30-08-19*/
 #include<iostream>
 using namespace std;
 template<class T>
